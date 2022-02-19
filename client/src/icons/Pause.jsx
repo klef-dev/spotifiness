1ca049d02ac2041a57718d4e8038d8265b77caae
klefcodes <abrahamugbeshe@gmail.com>
2022-02-19 22:51:15 +0100

    feat: completed design layout

---
+++ client/src/icons/Pause.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Icon(props) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export default Icon;
--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,6 +1,84 @@
 import React from "react";
 import "./style.css";
+import PlayIcon from "../../icons/Play";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="section">
+        <div className="section--header">
+          <div className="section--header--left">
+            <h2 className="title">Home</h2>
+          </div>
+          <div className="section--header--right">
+            <Link to="/" className="section--header--right--more">
+              VIEW ALL
+            </Link>
+          </div>
+        </div>
+        <div className="section--body">
+          <div className="section--body--item album--item">
+            <div className="section--body--item--img">
+              <img src="http://via.placeholder.com/150x150" alt="" />
+            </div>
+            <p className="section--body--item--title">Album name</p>
+            <p className="section--body--item--subtitle">
+              <Link to="/">Artist name</Link>
+            </p>
+            <div className="section--body--item--play">
+              <PlayIcon style={{ width: "24px", height: "24px" }} />
+            </div>
+          </div>
+
+          <div className="section--body--item artist--item">
+            <div className="section--body--item--img">
+              <img src="http://via.placeholder.com/150x150" alt="" />
+            </div>
+            <p className="section--body--item--title">LEX</p>
+            <p className="section--body--item--subtitle">Artist</p>
+            <div className="section--body--item--play">
+              <PlayIcon style={{ width: "24px", height: "24px" }} />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="section--header">
+          <div className="section--header--left">
+            <h2 className="title">Jump back in</h2>
+            <h3 className="subtitle">
+              What you listened most in the last months.
+            </h3>
+          </div>
+        </div>
+        <div className="section--body">
+          <div className="section--body--item album--item">
+            <div className="section--body--item--img">
+              <img src="http://via.placeholder.com/150x150" alt="" />
+            </div>
+            <p className="section--body--item--title">Album name</p>
+            <p className="section--body--item--subtitle">
+              <Link to="/">Artist name</Link>
+            </p>
+            <div className="section--body--item--play">
+              <PlayIcon style={{ width: "24px", height: "24px" }} />
+            </div>
+          </div>
+
+          <div className="section--body--item artist--item">
+            <div className="section--body--item--img">
+              <img src="http://via.placeholder.com/150x150" alt="" />
+            </div>
+            <p className="section--body--item--title">LEX</p>
+            <p className="section--body--item--subtitle">Artist</p>
+            <div className="section--body--item--play">
+              <PlayIcon style={{ width: "24px", height: "24px" }} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
